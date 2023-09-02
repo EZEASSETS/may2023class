cb9b26225f0e8e809ed9aaa629c6bfff0827e27d
kenneth Asogwa <kennethasogwa@kenneths-MacBook-Pro.local>
2023-09-02 09:59:04 +0100

    ken- test file name updated  with sufix Test

--- src/test/java/tests/SearchProductTest.java
+++ src/test/java/tests/SearchProductTest.java
@@ -1,18 +1,11 @@
 package tests;
 
 import generic.BaseTest;
-import generic.Element;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
 
-public class SearchProduct extends BaseTest {
+public class SearchProductTest extends BaseTest {
 
 
 
